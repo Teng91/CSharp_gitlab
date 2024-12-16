9b51dde622016f562c945f5126749c543b20c4d0
Teng91 <joy68304276@gmail.com>
2024-12-16 10:22:49 +0800

    first commit

---
+++ Lambda/Program.cs
@@ -0,0 +1,68 @@
+<<<<<<< HEAD
+﻿using System;
+
+namespace Combine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lambda(1.匿名方法 2.inline方法)
+            
+            // Func<int, int, int> func = new Func<int, int, int>((int a, int b) => { return a + b; });
+            // Func<int, int, int> func = new Func<int, int, int>((a, b) => { return a + b; });
+            // Func<int, int, int> func = (a, b) => { return a + b; };
+            Func<int, int, int> func = (a, b) => a + b; // 最簡寫法
+            int result = func(100, 200);
+            Console.WriteLine(result);
+
+            DoSomeCalc((a, b) => a * b, 100, 200); // 泛型委託可以自動推斷類型
+        }
+
+        // 有名字且不是inline的方法
+        // static int Add(int x, int y)
+        // {
+        //     return x + y;
+        // }
+
+        static void DoSomeCalc<T>(Func<T, T, T> func, T x, T y)
+        {
+            T res = func(x, y);
+            Console.WriteLine(res);
+        }
+    }
+=======
+﻿using System;
+
+namespace Combine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lambda(1.匿名方法 2.inline方法)
+            
+            // Func<int, int, int> func = new Func<int, int, int>((int a, int b) => { return a + b; });
+            // Func<int, int, int> func = new Func<int, int, int>((a, b) => { return a + b; });
+            // Func<int, int, int> func = (a, b) => { return a + b; };
+            Func<int, int, int> func = (a, b) => a + b; // 最簡寫法
+            int result = func(100, 200);
+            Console.WriteLine(result);
+
+            DoSomeCalc((a, b) => a * b, 100, 200); // 泛型委託可以自動推斷類型
+        }
+
+        // 有名字且不是inline的方法
+        // static int Add(int x, int y)
+        // {
+        //     return x + y;
+        // }
+
+        static void DoSomeCalc<T>(Func<T, T, T> func, T x, T y)
+        {
+            T res = func(x, y);
+            Console.WriteLine(res);
+        }
+    }
+>>>>>>> 67b25b8b4111586566563a787076eef26a037169
+}
